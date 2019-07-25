cea7ed3dcd7c0531b9f526381693497e2a9352e7
Walt Zimmerman <zimmermanw84@gmail.com>
2019-07-25 13:19:35 -0700

    we are done ship it

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
-console.log("HELLO WORLD");
+const figlet = require('figlet');
+const arg = require('arg');
+const fs = require('fs');
+const WRITE_COMMAND_FILE_PATH = './commands/';
+// const APP_ROOT
+
+const introMessage = figlet.textSync('AuditBoard', {
+  font: 'Slant',
+  horizontalLayout: 'default',
+  verticalLayout: 'default',
+});
+console.log(introMessage);
+
+// arg parsing
+
+function parseArgs(rawArgs) {
+  const args = arg(
+    {
+      // 'make:command': String,
+      '--yes': Boolean,
+      '--install': Boolean,
+      '-g': '--git',
+      '-y': '--yes',
+      '-i': '--install',
+    },
+    {
+      argv: rawArgs.slice(3),
+    }
+    );
+    
+    return {
+      commandName: args['make:command'],
+      git: args['--git'] || false,
+      template: args._[0],
+      runInstall: args['--install'] || false,
+    };
+  }
+  
+  function parseCommand(command) {
+    // some
+    const fileName = command.replace(':', '_');
+    const commandExists = fs.existsSync(`./commands/${fileName}.js`);
+    if (commandExists) {
+      const callback = require(`./commands/make_command`);
+      console.log("HOPEFULLY A FN", callback);
+      // validate
+      // look for command file
+      // return command function
+      return callback;
+    }
+    else {
+      throw new Error('dummy');
+    }
+  }
+  
+  module.exports = function cli(args) {
+    const options = parseArgs(args);
+    const inputCommandString = args[2];
+    const command = parseCommand(inputCommandString);
+    command(options);
+    // console.log(command);
+    
+  // if (args['make:command'] && !args['make:command'].includes(':')) {
+  //   throw new Error("New commands must be prefixed with a namespace, eg 'auditboard make:command generate:model'");
+  // }
+  
+};
---
+++ commands/make_command.js
@@ -0,0 +1,3 @@
+module.exports = (args) => {
+  console.log("HELLO WORLD", args);
+};
