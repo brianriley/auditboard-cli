0e046769855fb2e84c881e83da1b9c3e25fe2fad
Walt Zimmerman <zimmermanw84@gmail.com>
2019-07-26 08:17:02 -0700

    fix pathing and bug found while running in remote repo

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
   // will have to fix to work for using in the app root
-  APP_COMMAND_FILE_PATH: path.resolve('../../cli-commands'),
-  LOCAL_FILE_PATH: path.resolve('./commands'),
+  APP_COMMAND_FILE_PATH: path.resolve('./cli-commands'),
+  LOCAL_FILE_PATH: path.resolve('./node_modules/auditboard-cli/commands'),
 };
